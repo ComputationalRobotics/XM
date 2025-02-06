32f8ed5de9b4f0aa13aec28378145d657e106945
haoyu <hyhan@seas.harvard.edu>
2025-02-05 23:15:18 -0500

    add

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var NUM_INTERP_FRAMES = 666;
 
 var interp_images = [];
 function preloadInterpolationImages() {
@@ -31,7 +31,7 @@ $(document).ready(function() {
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 3,
+			slidesToShow: 1,
 			loop: true,
 			infinite: true,
 			autoplay: false,
