4f028bb2f4550e3642e6b1ca260979cd80722609
haoyu <hyhan@seas.harvard.edu>
2025-02-07 22:29:41 -0500

    add

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,31 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 100;
 
-var interp_images = [];
-function preloadInterpolationImages() {
+var interpImagesDict = {}; // 用于存储不同 INTERP_BASE 的图片数组
+
+function preloadInterpolationImages(INTERP_BASE) {
+  // 如果该 INTERP_BASE 已经存在，则不需要重新加载
+  if (interpImagesDict[INTERP_BASE]) return;
+
+  interpImagesDict[INTERP_BASE] = []; // 创建新的数组
+
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
+    var path = "./static/interpolation/" + INTERP_BASE + "/stacked/" + String(i).padStart(6, '0') + '.jpg';
+    var img = new Image();
+    img.src = path;
+    interpImagesDict[INTERP_BASE].push(img);
   }
 }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
+function setInterpolationImage(INTERP_BASE, i, index) {
+  var images = interpImagesDict[INTERP_BASE]; // 获取对应的图片数组
+  if (!images || !images[i]) return; // 如果图片未加载，则不进行更新
+
+  var image = images[i];
   image.ondragstart = function() { return false; };
   image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
+  $('#interpolation-image-wrapper-' + String(index)).empty().append(image);
 }
 
 
@@ -65,13 +74,21 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    preloadInterpolationImages("replica1");
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
+    $('#interpolation-slider-1').on('input', function(event) {
+      setInterpolationImage("replica1",this.value,1);
     });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    setInterpolationImage("replica1",0,1);
+    $('#interpolation-slider-1').prop('max', NUM_INTERP_FRAMES - 1);
+
+    preloadInterpolationImages("replica2");
+
+    $('#interpolation-slider-2').on('input', function(event) {
+      setInterpolationImage("replica2",this.value,2);
+    });
+    setInterpolationImage("replica2",0,2);
+    $('#interpolation-slider-2').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
